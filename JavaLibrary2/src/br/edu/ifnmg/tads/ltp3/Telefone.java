10a51ca35c70643881b965568c525daec6925ca3
aluno <aluno@aluno-desktop>
2013-03-21 08:38:25 -0300

    aula sobre javabeans

---
+++ JavaLibrary2/src/br/edu/ifnmg/tads/ltp3/Telefone.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.ltp3;
+
+/**
+ *
+ * @author aluno
+ */
+public class Telefone {
+    private int id;
+    private int ddd;
+    private int numero;
+    
+    public Telefone(){
+       id = 0;
+       ddd = 0;
+       numero = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+}
